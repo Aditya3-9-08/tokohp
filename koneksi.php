651ad3edd723b8e9b29e90a9b3afe562b4f61fcc
Aditya3-9-08 <adityaharis312@gmail.com>
2026-05-20 09:09:56 +0700

    teskan

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "ojokerro_chale"; 
 $pass = "chaleya0319";     
-$db   = "toko_hp";
+$db   = "ojokerro_tokohp";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
